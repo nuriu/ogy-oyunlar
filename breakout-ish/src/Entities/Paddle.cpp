9c61cc99710a65137065571d535b4c3ccd16bd32
Nuri Uzunoğlu <nuriu@protonmail.com>
2018-11-16 21:25:05 +0300

    [breakout-ish] Paddle.cpp this-> eklemeleri yapıldı.

--- breakout-ish/src/Entities/Paddle.cpp
+++ breakout-ish/src/Entities/Paddle.cpp
@@ -5,40 +5,41 @@ Paddle::Paddle(const CoreComponents& components, const unsigned int selectedPadd
 {
     if (selectedPaddleIndex < 3)
     {
-        setTexture(m_Components.m_AssetManager->getTexture("paddles"));
-        setTextureRect(sf::IntRect(0, selectedPaddleIndex * 24, 100, 24));
+        this->setTexture(m_Components.m_AssetManager->getTexture("paddles"));
+        this->setTextureRect(sf::IntRect(0, selectedPaddleIndex * 24, 100, 24));
     }
     else
     {
-        setTexture(m_Components.m_AssetManager->getTexture("paddles-long"));
-        setTextureRect(sf::IntRect(0, (selectedPaddleIndex - 3) * 24, 124, 24));
+        this->setTexture(m_Components.m_AssetManager->getTexture("paddles-long"));
+        this->setTextureRect(sf::IntRect(0, (selectedPaddleIndex - 3) * 24, 124, 24));
     }
 }
 
 void Paddle::initialize()
 {
-    setOrigin(getLocalBounds().width / 2.0f, getLocalBounds().height / 2.0f);
-    setPosition(m_Components.m_RenderWindow->getSize().x / 2.0f,
-                m_Components.m_RenderWindow->getSize().y - 25.0f);
+    this->setOrigin(this->getLocalBounds().width / 2.0f, this->getLocalBounds().height / 2.0f);
+    this->setPosition(m_Components.m_RenderWindow->getSize().x / 2.0f,
+                      m_Components.m_RenderWindow->getSize().y - 25.0f);
 
-    m_Width  = getLocalBounds().width;
-    m_Height = getLocalBounds().height;
+    m_Width  = this->getLocalBounds().width;
+    m_Height = this->getLocalBounds().height;
 }
 
 void Paddle::processInput()
 {
     if (m_Components.m_InputManager->isKeyPressed(sf::Keyboard::Left))
     {
-        if (getPosition().x - m_Width / 2.0f > 10.0f)
+        if (this->getPosition().x - m_Width / 2.0f > 10.0f)
         {
-            move(m_PaddleSpeed * m_Components.m_TimePerFrame->asSeconds() * -1.0f, 0.0f);
+            this->move(m_PaddleSpeed * m_Components.m_TimePerFrame->asSeconds() * -1.0f, 0.0f);
         }
     }
     else if (m_Components.m_InputManager->isKeyPressed(sf::Keyboard::Right))
     {
-        if (getPosition().x + m_Width / 2.0f < m_Components.m_RenderWindow->getSize().x - 10.0f)
+        if (this->getPosition().x + m_Width / 2.0f <
+            m_Components.m_RenderWindow->getSize().x - 10.0f)
         {
-            move(m_PaddleSpeed * m_Components.m_TimePerFrame->asSeconds(), 0.0f);
+            this->move(m_PaddleSpeed * m_Components.m_TimePerFrame->asSeconds(), 0.0f);
         }
     }
 }
